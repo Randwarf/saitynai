5288fe30bed74b2f5b8fd81da9a056bc78e8c537
Randwarf <matas.vaitkevicius@gmail.com>
2023-12-09 13:43:06 +0200

    BuildingPosition migration

---
+++ API/Migrations/20231209114220_BuildingPosition.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class BuildingPosition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "X",
+                table: "Buildings",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Y",
+                table: "Buildings",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "X",
+                table: "Buildings");
+
+            migrationBuilder.DropColumn(
+                name: "Y",
+                table: "Buildings");
+        }
+    }
+}
